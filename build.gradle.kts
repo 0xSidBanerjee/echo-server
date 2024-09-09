cc50be181155c93d9cc7d829b8b4bfcc5b01717c
Siddhanta Banerjee <siddhantban02@gmail.com>
2024-09-09 10:56:26 +0530

    First commit
    Completed setting up the environment for Echo Server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     id("java")
+    application
+}
+
+application{
+    mainClass.set("org.acdc.Main")
 }
 
 group = "org.acdc"
@@ -10,10 +15,32 @@ repositories {
 }
 
 dependencies {
+    //JUnit
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    // Slf4j
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+
+    // JCommander
+    implementation("org.jcommander:jcommander:2.0")
+
+    // Mockito
+    testImplementation("org.mockito:mockito-core:5.13.0")
 }
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.acdc.Main"
+    }
+}
 tasks.test {
     useJUnitPlatform()
 }
